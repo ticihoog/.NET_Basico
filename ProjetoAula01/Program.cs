7b5a6d76faabdb23cdaaec7654a8b396cb067432
ticihoog <ticianapaternostro@gmail.com>
2023-11-09 17:11:26 -0300

    alterações de nome - aula01

---
+++ ProjetoAula01/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("**** BEM VINDO(A)! ****");
+Console.WriteLine("Residência CEPEDI - .NET Básico");
+Console.WriteLine("      Aula 01");
