d312cfcc30751101a82f2125499190669ae638c5
ticihoog <ticianapaternostro@gmail.com>
2023-11-30 15:56:24 -0300

    prova_ind_alteracao_002

--- Atividades /NET-PROVA_IND/Program.cs
+++ Atividades /NET-PROVA_IND/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+{
+List<Advogado> advogados = new List<Advogado>();
+List<Cliente> clientes = new List<Cliente>();
+
+advogados.Add(new Advogado("Ricardo Teixeira", new DateTime (1990, 5, 15), "12345678901", "CNA123"));
+advogados.Add(new Advogado("Dan Sampaio", new DateTime (1985, 8, 20), "98765432101", "CNA456"));
+
+clientes.Add(new Cliente("Rita Santos", new DataTime(1988, 4, 21), "02538968518", "solteria", "professora"));
+clientes.Add(new Cliente("Isabela Chaves", new DataTime(1986, 2 ,4), "0151871507", "casada" , "medica" ));
+}
